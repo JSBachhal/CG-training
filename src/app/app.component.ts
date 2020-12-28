fe107346e0f1180c943f5bf49453c2a7f1aacd5f
JSBachhal <jassisngh11@gmail.com>
2020-12-28 13:06:13 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { AfterViewInit, Component, ElementRef,  VERSION, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('can', { static: false }) canvas: ElementRef<HTMLCanvasElement>;
+  context: RenderingContext | CanvasRenderingContext2D;
+  ngAfterViewInit() {
+    this.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+    this.context.fillStyle = '#ff3';
+
+    this.context.strokeRect(110, 110, 100, 100);
+    this.context.fillRect(110, 110, 100, 100);
+    this.gameloop();
+  }
+
+  gameloop() {
+
+  }
 }
